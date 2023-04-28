1c589b9e7c308d2ab0e5b5d4dd26d3f522e7e3a9
saashimi <kevin@saavedra.io>
2023-04-28 16:42:03 -0700

    Implement recursive loop through input directory

--- main.js
+++ main.js
@@ -8,9 +8,24 @@ async function handleDirOpen() {
     if (canceled) {
         return "Directory is not selected";
     } else {
-        return filePaths[0]
-    }
-}
+        return filePaths[0];
+    };
+};
+
+const getFiles = async function(dirPath, fileArray) {
+    // recursively loop through folders and get file paths
+    files = fs.readdirSync(dirPath);
+    fileArray = fileArray || [];
+  
+    files.forEach(function(file) {
+      if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+        fileArray = getFiles(dirPath + "/" + file, fileArray);
+      } else {
+        fileArray.push(path.join(dirPath, "/", file));
+      };
+    });
+    return fileArray;
+};
 
 async function handleCopyFiles (event, copyFrom, copyTo) {
     const win = BrowserWindow.getFocusedWindow();
@@ -18,64 +33,51 @@ async function handleCopyFiles (event, copyFrom, copyTo) {
     
     try {
         // Get the files as an array
-        const files = await fs.promises.readdir( copyFrom );
+        const files = await getFiles( copyFrom );
         let incr = files.length / 100;
         let copied = 0;
 
-        // Loop them all with the new for...of
         for( const file of files ) {            
-            // Get the full paths
-            const fromPath = path.join( copyFrom, file );
+            
+            const stat = await fs.promises.stat( file );
+            const birthTime = stat.birthtime;
+            
+            // TODO: instead of relying on birthtime, extract the EXIF data
+            let dateTimeString = birthTime.toString().split(" ");
+            
+            const months = {Jan: '01', Feb: '02', Mar: '03', Apr: '04', May: '05', Jun: '06',
+                            Jul: '07', Aug: '08', Sep: '09', Oct: '10', Nov: '11', Dec: '12'};
+            
+            //TODO: why can't this be destructured?
+            let year = dateTimeString[3];
+            let month = months[dateTimeString[1]];
+            let day = dateTimeString[2];
+            const folder = `${year}\-${month}\-${day}`;
+            let filename = path.parse(file).base;
+            const toPath = path.join( copyTo, year, folder, filename);
+            
+            // Create year folder
+            if (!fs.existsSync(path.join( copyTo, year ))) {
+                fs.mkdirSync(path.join( copyTo, year ));
+            };
+            // Create date folder if does not exist and copy files
+            if (!fs.existsSync(path.join( copyTo, year, folder ))){
+                fs.mkdirSync(path.join( copyTo, year, folder ));
+                await fs.promises.copyFile( file, toPath );
 
-            // Stat the file to see if we have a file or dir
-            const stat = await fs.promises.stat( fromPath );
-
-            if( stat.isFile() )
-                console.log( "'%s' is a file.", fromPath );
-            else if( stat.isDirectory() )
-                console.log( "'%s' is a directory.", fromPath );
-
-            // Now copy async
-            if ( stat.isFile() ) {
-                const birthTime = stat.birthtime;
-                
-                // TODO: instead of relying on birthtime, extract the EXIF data
-                let dateTimeString = birthTime.toString().split(" ");
-                
-                const months = {Jan: '01', Feb: '02', Mar: '03', Apr: '04', May: '05', Jun: '06',
-                                Jul: '07', Aug: '08', Sep: '09', Oct: '10', Nov: '11', Dec: '12'};
-                
-                //TODO: why can't this be destructured?
-                let year = dateTimeString[3];
-                let month = months[dateTimeString[1]];
-                let day = dateTimeString[2];
-                const folder = `${year}\-${month}\-${day}`;
-                const toPath = path.join( copyTo, year, folder, file );
-                
-                // Create year folder
-                if (!fs.existsSync(path.join( copyTo, year ))) {
-                    fs.mkdirSync(path.join( copyTo, year ));
-                };
-                // Create date folder if does not exist and copy files
-                if (!fs.existsSync(path.join( copyTo, year, folder ))){
-                    fs.mkdirSync(path.join( copyTo, year, folder ));
-                    await fs.promises.copyFile( fromPath, toPath );
-
-                } else {
-                    // Copy files if destination folders exist
-                    await fs.promises.copyFile( fromPath, toPath );
-                };
-                console.log( "Copied '%s'->'%s'", fromPath, toPath );
-                copied += 1;
-                incr += incr;
-                if (incr <= .99) {
-                    win.setProgressBar(incr);
-                } else {
-                    // Set a delay so completion is more visible
-                    win.setProgressBar(incr += 1000);
-                    win.setProgressBar(-1);
-                };
-
+            } else {
+                // Copy files if destination folders exist
+                await fs.promises.copyFile( file, toPath );
+            };
+            console.log( "Copied '%s'->'%s'", file, toPath );
+            copied += 1;
+            incr += incr;
+            if (incr <= .99) {
+                win.setProgressBar(incr);
+            } else {
+                // Set a delay so completion is more visible
+                win.setProgressBar(incr += 1000);
+                win.setProgressBar(-1);
             };
         }; // End for...of
         return copied;

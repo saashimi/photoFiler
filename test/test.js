688c79e8d43f64a2dd0d7f2b9d0b2d8649d71aec
saashimi <kevin@saavedra.io>
2023-05-14 09:35:20 -0700

    Selenium test script

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
-const webdriver = require('selenium-webdriver')
-const assert = require('assert')
-const driver = new webdriver.Builder()
+const {By, Builder, Browser} = require('selenium-webdriver');
+const {suite} = require("selenium-webdriver/testing")
+const assert = require("assert")
+/*const driver = new webdriver.Builder()
   // The "9515" is the port opened by ChromeDriver.
   .usingServer('http://localhost:9515')
   .withCapabilities({
@@ -10,12 +11,35 @@ const driver = new webdriver.Builder()
     }
   })
   .forBrowser('chrome') // note: use .forBrowser('electron') for selenium-webdriver <= 3.6.0
-  .build()
+  .build() */
 
-  driver.wait(() => {
-    return driver.getTitle().then((title) => {
-      assert.equal(title, '📷 Photo 🗄️ Filer')
-      return
-    })
-  }, 1000)
-  driver.quit()
+
+suite(function (env) {
+    describe('First script', function () {
+      let driver;
+      
+      const documentInitialized = () => driver.executeScript('return initialized');
+
+      before(async function () {
+        driver = await new Builder()
+        // The "9515" is the port opened by ChromeDriver.
+        .usingServer('http://localhost:9515')
+        .withCapabilities({
+            'goog:chromeOptions': {
+            // Here is the path to your Electron binary.
+            binary: 'release-builds/photofiler-darwin-x64/photofiler.app/Contents/MacOS/photofiler'
+            }
+        })
+        .forBrowser('chrome') // note: use .forBrowser('electron') for selenium-webdriver <= 3.6.0
+        .build();        
+      });
+  
+      after(async () => await driver.quit());
+  
+      it('Check Initialized', async function () {  
+        let title = await driver.getTitle();
+        assert.equal("📷 Photo 🗄️ Filer", title);
+
+      });
+    });
+  }, { browsers: [Browser.CHROME]});
